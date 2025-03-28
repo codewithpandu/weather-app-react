ccefe9f0cdc0de9d32b72e43d858d8058e1e17fd
Pandu Setia Darmawan <pandusetiadarmawan01@gmail.com>
2025-03-28 09:46:44 +0700

    fix responsive

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Weather from "./Weather";
 
 function App() {
   return (
-    <div className="bg-zinc-300 h-screen grid justify-center place-content-center">
+    <div className="bg-zinc-300 p-4 h-screen grid justify-center place-content-center">
       <Weather />
     </div>
   );
